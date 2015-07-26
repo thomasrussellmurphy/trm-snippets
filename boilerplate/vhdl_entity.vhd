67b34d58f7af0f5bbf33d9cfeba9b40f9b7d07c7
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2015-07-25 22:55:00 -0500

    Update IEEE package includes to better format

--- boilerplate/vhdl_entity.vhd
+++ boilerplate/vhdl_entity.vhd
@@ -1,7 +1,7 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.std_logic_arith.all;
+  use ieee.std_logic_unsigned.all;
 
 entity vhdl_entity is
   port (
