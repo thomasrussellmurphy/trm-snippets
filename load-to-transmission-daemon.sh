a664f7b63a41d75e346c5d2058c68a40db8b0875
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2016-07-07 00:31:02 -0400

    Correct usage of `if` syntax in torrent loading script

--- load-to-transmission-daemon.sh
+++ load-to-transmission-daemon.sh
@@ -3,10 +3,10 @@
 # using transmission-remote and environment authentication
 # Could also use '--netrc' with corresponding external configuration
 
-if [ -z "$TR_AUTH" ]; do
+if [ -z "$TR_AUTH" ]; then
 	echo "Must set TR_AUTH in local environment to access transmission-daemon"
 	exit 1
-done
+fi
 
 for filename in "$1"; do
     transmission-remote --authenv --add "filename"
